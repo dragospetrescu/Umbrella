e4699d231d960df3c4569dab8664e260b366b61a
dragos <petrescudragos1995@gmail.com>
2018-06-26 14:00:47 +0300

    refactored code so it uses current location for weather get

--- app/src/main/java/com/alpaca/umbrella/location/LocationManager.kt
+++ app/src/main/java/com/alpaca/umbrella/location/LocationManager.kt
@@ -7,19 +7,18 @@ import android.support.v4.app.ActivityCompat
 import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.app.Activity
 import android.util.Log
+import com.alpaca.umbrella.weather.WeatherManager
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 
-class LocationManager constructor(activity: Activity) {
+class LocationManager constructor(private var activity: Activity, private var weatherManager: WeatherManager) {
 
     private lateinit var client: FusedLocationProviderClient
-    private var activity: Activity = activity
 
     fun init() {
         requestPermission()
 
         this.client = LocationServices.getFusedLocationProviderClient(activity)
-
 
     }
 
@@ -28,22 +27,28 @@ class LocationManager constructor(activity: Activity) {
         if (ActivityCompat.checkSelfPermission(activity, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(activity, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.d("ERROR", "NO PERMISSIONS")
-            return
+            requestPermission()
         }
 
         this.client.lastLocation.addOnFailureListener {
-            Log.d("ERROR", "PICAT " + it.stackTrace)
+            Log.d("ERROR", it.stackTrace.toString())
         }
 
         this.client.lastLocation.addOnSuccessListener {
-            Log.d("MERE", it.latitude.toString() + " " + it.longitude.toString())
-        }
 
+            Log.d("DEBUG", "Got location: " + it.latitude.toString() + " " + it.longitude.toString())
+            weatherManager.getWeatherFromAPI(null, it.latitude, it.longitude)
+        }
     }
 
 
     private fun requestPermission() {
-        Log.d("MERE", "PERMISIUNI")
-        ActivityCompat.requestPermissions(activity, arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION), 1);
+        Log.d("DEBUG", "Asking for permissions")
+        ActivityCompat.requestPermissions(activity, arrayOf(ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION), 1)
+
+        if (ActivityCompat.checkSelfPermission(activity, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(activity, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermission()
+        }
     }
 }
---
+++ app/src/main/java/com/alpaca/umbrella/weather/WeatherManager.kt
@@ -0,0 +1,68 @@
+package com.alpaca.umbrella.weather
+
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class WeatherManager constructor(private var applicationContext: Context) {
+
+
+    private lateinit var api : OpenWeatherAPI
+    private lateinit var retrofit: Retrofit
+    private var messageFactory: MessageFactory = MessageFactory()
+
+    fun init() {
+        val apiClient = OkHttpClient.Builder()
+                .addInterceptor(OpenWeatherInterceptor()).build()
+
+        retrofit = Retrofit.Builder().apply {
+            baseUrl(OpenWeatherAPI.BASE_URL)
+            addConverterFactory(GsonConverterFactory.create())
+            client(apiClient)
+        }.build()
+
+        api = retrofit.create(OpenWeatherAPI::class.java)
+    }
+
+    fun getWeatherFromAPI(cityName : String? = null, latitude : Double? = null,
+                                  longitude: Double? = null) {
+
+        val call: Call<WeatherResponse>
+
+        if (cityName != null)
+            call = api.getForecast(cityName, 1)
+        else if (latitude != null && longitude != null)
+            call = api.getForecast(latitude, longitude, 1)
+        else {
+            Toast.makeText(applicationContext, "Error", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        call.enqueue(object : Callback<WeatherResponse> {
+            override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                val res: WeatherResponse? = response.body()
+
+                Log.d("RESPONSE", response.toString())
+                Log.d("RESPONSE BODY", response.body()?.toString())
+
+                if (response.isSuccessful) {
+                    Log.d("TEMPERATURE", res?.forecast!![0].main.temp.toString())
+                    Toast.makeText(applicationContext, messageFactory.get(res), Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(applicationContext, "Unsuccessful call to weather API", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherResponse>?, t: Throwable) {
+                Toast.makeText(applicationContext, "Error", Toast.LENGTH_LONG).show()
+                t.printStackTrace()
+            }
+        })
+    }
+}
